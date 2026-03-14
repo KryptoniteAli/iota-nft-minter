6e9a7a13e01b7b0afa6d648950da0caadc730b34
Kryptonite Ali <cryptonite.ali@gmail.com>
2026-03-14 12:47:34 +0800

    rename app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-export default function App() {
+\export default function App() {
   return (
     <div style={{ padding: 30, background: "#fff", minHeight: "100vh", color: "#000" }}>
-      <h1>Hello from IOTA Minter</h1>
+      <h1>Hello from Kryptonite minter</h1>
       <button onClick={() => alert("works")}>Test Button</button>
     </div>
   );
