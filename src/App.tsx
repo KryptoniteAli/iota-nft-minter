04a752595bc4108ff87797f56f9692d2ef1813e5
Kryptonite Ali <cryptonite.ali@gmail.com>
2026-03-14 12:54:08 +0800

    fix invalid character

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-\export default function App() {
+export default function App() {
   return (
     <div style={{ padding: 30, background: "#fff", minHeight: "100vh", color: "#000" }}>
       <h1>Hello from Kryptonite minter</h1>
